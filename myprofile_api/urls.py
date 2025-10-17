b5a25b88f38df25ba1833fe946cdf338b825bd2e
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-17 10:14:46 +0100

    New Changes for deployment

--- myprofile_api/urls.py
+++ myprofile_api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import ProfileView
 
+def api_root(request):
+    return JsonResponse({"message": "Welcome to the API Home!"})
+
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('me/', ProfileView.as_view(), name='profile'),
 ]
+
